68fa6af6afa1d074e47c8742675721b16c320b28
Mike Peiman <mikepeiman@gmail.com>
2022-03-23 12:44:25 -0400

    brought in some old Vue code for radial menu to rehash but left it, began simple new implementation for operations select

---
+++ src/components/RadialMenu.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Icon from '@iconify/svelte';
+$: selectedOperation = ''
+$: console.log(`🚀 ~ file: RadialMenu.svelte ~ line 4 ~ selectedOperation`, selectedOperation)
+
+	let operations = [
+		{
+			name: 'Multiply',
+			symbol: '*',
+			iconname: 'fa-solid:times'
+		},
+		{
+			name: 'Subtract',
+			symbol: '-',
+			iconname: 'fa-solid:minus'
+		},
+		{
+			name: 'Add',
+			symbol: '+',
+			iconname: 'fa-solid:plus'
+		},
+		{
+			name: 'Divide',
+			symbol: '/',
+			iconname: 'fa-solid:divide'
+		},
+		{
+			name: 'Equals',
+			symbol: '=',
+			iconname: 'fa-solid:equals'
+		}
+	];
+
+	function operationSelect(operation) {
+		console.log(
+			`🚀 ~ file: RadialMenu.svelte ~ line 33 ~ operationSelector ~ operation`,
+			operation
+		);
+        selectedOperation = operation
+	}
+</script>
+
+{#each operations as operation}
+	<!-- <div class="btn" on:click={() => operationSelect(operation)} /> -->
+	<!-- <i class="fas" :class="operation.iconname" v-model="selectedOperation"></i> -->
+	<div class="p-2 bg-winterblues-800 rounded-full {selectedOperation.name === operation.name ? 'bg-red-500' : ''} transition-all"  on:click={() => operationSelect(operation)}>
+		<Icon icon={operation.iconname} />
+	</div>
+{/each}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,25 @@
 <script>
+	import RadialMenu from '$components/RadialMenu.svelte';
+	import Icon from '@iconify/svelte';
 	import { onMount } from 'svelte';
+	import Select from 'svelte-select';
 
-	// create functions for math operations
+	let icons = {
+		multiply: 'majesticons:multiply',
+		divide: 'majesticons:divide',
+		plus: 'majesticons:plus',
+		minus: 'majesticons:minus',
+		equal: 'fa-solid:equals',
+		close: 'majesticons:close'
+	};
+
+	let operations = [
+		{ value: 'multiply', label: 'Multiply' },
+		{ value: 'divide', label: 'Divide' },
+		{ value: 'plus', label: 'Plus' },
+		{ value: 'minus', label: 'Minus' }
+	];
+
 	// create add function
 	const add = (a, b) => a + b;
 	// create subtract function
@@ -20,35 +38,55 @@
 		result = multiply(valueA, valueB);
 	});
 
+	function processCalculation() {
+		// get the operator
+		const operator = this.getAttribute('data-operator');
+		console.log(`🚀 ~ file: index.svelte ~ line 36 ~ processCalculation ~ operator`, operator);
+		// get the result
+		result = eval(`${valueA} ${operator} ${valueB}`);
+	}
+
 	function newRandomValues() {
 		valueA = Math.ceil(Math.random() * 100);
 		valueB = Math.ceil(Math.random() * 100);
 		result = multiply(valueA, valueB);
 	}
+
+	function handleSelect() {}
+	function handleClear() {}
 </script>
 
 <div class="flex w-full h-full items-center justify-center">
-	<div class="flex flex-col p-4 bg-winterblues-900 w-[50%] h-[50%] items-center justify-center">
+	<div
+		class="flex flex-col p-4 bg-winterblues-900 w-[50%] h-[50%] items-center justify-center border-2 border-lime-500 border-opacity-50"
+	>
 		<button on:click={newRandomValues} class="p-4 bg-winterblues-800">New values</button>
 		<div class="flex">
 			<input
 				type="text"
 				bind:value={valueA}
-				class="outline-none ring-none border-l-0 border-t-0 border-r-0 p-3 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
+				on:blur={processCalculation}
+				class="text-center outline-none ring-none border-l-0 border-t-0 border-r-0 p-0 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
 				placeholder="value A"
 			/>
-			<div class="flex items-center justify-center">X</div>
+			<div class="flex flex-col items-center justify-center">
+				<RadialMenu />
+			</div>
 			<input
 				type="text"
 				bind:value={valueB}
-				class="outline-none ring-none border-l-0 border-t-0 border-r-0 p-3 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
+				on:blur={processCalculation}
+				class="text-center outline-none ring-none border-l-0 border-t-0 border-r-0 p-3 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
 				placeholder="value B"
 			/>
-			<div class="flex items-center justify-center">=</div>
+			<div class="flex items-center justify-center">
+				<!-- <Icon icon={icons.equal} class="text-4xl text-lime-500" /> -->
+				<Icon icon={icons.equal2} class="text-2xl text-lime-500" />
+			</div>
 			<input
 				type="text"
 				bind:value={result}
-				class="outline-none ring-none border-l-0 border-t-0 border-r-0 p-3 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
+				class="text-center outline-none ring-none border-l-0 border-t-0 border-r-0 p-3 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
 				placeholder="value C"
 			/>
 		</div>
@@ -56,8 +94,10 @@
 </div>
 
 <style lang="scss" global>
-	input {
+	input[type='text'] {
 		outline: 2px;
+		width: 8ch;
+		font-size: 2.5rem;
 		:focus {
 			outline: none;
 			border: none;
