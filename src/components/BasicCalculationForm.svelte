9e2ff0473a559da323b38b9b58eb647b4a82a808
Mike Peiman <mikepeiman@gmail.com>
2022-03-24 14:26:28 -0400

    updated input styles - text size and width

--- src/components/BasicCalculationForm.svelte
+++ src/components/BasicCalculationForm.svelte
@@ -12,33 +12,33 @@
 	};
     </script>
 
-<div class="flex flex-col justify-center items-center">
+<div class="flex flex-col justify-center items-center text-4xl">
     <div class="flex">
         <input
             type="text"
             bind:value={$valueA}
             on:blur={processCalculation}
-            class="text-center outline-none ring-none border-l-0 border-t-0 border-r-0 p-0 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
+            class="basic-underline-text-input"
             placeholder="value A"
         />
-        <div class="flex flex-col items-center justify-center text-lime-500">
+        <div class="flex flex-col items-center justify-center text-lime-500 mx-2">
             <Icon icon={$selectedOperation.iconname || icons.times} class="text-3xl" />
         </div>
         <input
             type="text"
             bind:value={$valueB}
             on:blur={processCalculation}
-            class="text-center outline-none ring-none border-l-0 border-t-0 border-r-0 p-3 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
+            class="basic-underline-text-input"
             placeholder="value B"
         />
-        <div on:click={processCalculation} class="flex items-center justify-center">
+        <div on:click={processCalculation} class="flex items-center justify-center mx-2">
             <!-- <Icon icon={icons.equal} class="text-4xl text-lime-500" /> -->
-            <Icon icon={icons.equals} class="text-3xl text-lime-500" />
+            <Icon icon={icons.equals} class="text-3xl text-lime-500 " />
         </div>
         <input
             type="text"
             bind:value={$result}
-            class="text-center outline-none ring-none border-l-0 border-t-0 border-r-0 p-3 m-2 border-b-2 border-b-lime-500 bg-none bg-transparent focus:ring-0 focus:outline-none"
+            class="basic-underline-text-input"
             placeholder="value C"
         />
     </div>
