a648220e5b1269911bb67f10c9c4adf5c35dd564
Mike Peiman <mikepeiman@gmail.com>
2022-03-24 20:14:10 -0400

    added active store and style for topnav items

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -1,30 +1,46 @@
 <script>
 	import Icon from '@iconify/svelte';
-    import tooltip from '$utils/tooltip';
+	import tooltip from '$utils/tooltip';
+	import { currentView } from '$stores/stores';
+    import { get } from 'svelte/store';
+ $: current = get(currentView)
+//  $: current
+//  $: console.log(`🚀 ~ file: TopNav.svelte ~ line 8 ~ current`, current)
 	const icons = {
 		'equalizer-1': 'ph:equalizer-bold',
 		'equalizer-2': 'mdi:equalizer',
 		'equalizer-3': 'icon-park-outline:equalizer',
 		'list-view': 'dashicons:list-view',
-        'grid-view': 'dashicons:grid-view',
-        'grid-view-2': 'ic:baseline-view-comfy',
-        'carousel': 'ic:outline-view-carousel',
-        'calculator-2': 'ic:round-table-view',
-        'calculator-3': 'bi:calculator-fill',
-        'calculator-4': 'ant-design:calculator-filled',
-        'calculator-5': 'clarity:calculator-solid',
-        'quiz': 'ic:baseline-quiz',
-        'quiz-2': 'fluent:quiz-new-28-filled',
-        'user-profile': 'bxs:user-circle',
+		'grid-view': 'dashicons:grid-view',
+		'grid-view-2': 'ic:baseline-view-comfy',
+		carousel: 'ic:outline-view-carousel',
+		'calculator-2': 'ic:round-table-view',
+		'calculator-3': 'bi:calculator-fill',
+		'calculator-4': 'ant-design:calculator-filled',
+		'calculator-5': 'clarity:calculator-solid',
+		quiz: 'ic:baseline-quiz',
+		'quiz-2': 'fluent:quiz-new-28-filled',
+		'user-profile': 'bxs:user-circle'
 	};
+
+	function navigate(icon) {
+		console.log(`🚀 ~ file: TopNav.svelte ~ line 24 ~ navigate ~ icon`, icon);
+        current = icon
+		currentView.set(icon);
+	}
 </script>
 
 <div id="header" class="flex w-full items-center justify-center bg-winterblues-900">
     {#each Object.keys(icons) as icon}
-	<div class="tooltip flex items-center justify-center text-4xl mx-2 hover:text-winterblues-400 hover:cursor-pointer transition-all"
-    use:tooltip
-    title="{icons[icon]}">
-        <Icon icon={icons[icon] }/>
-    </div>
-    {/each}
+    
+		<div
+			class="tooltip flex items-center justify-center text-4xl mx-2 hover:text-winterblues-400 hover:cursor-pointer transition-all
+            {current === icon ? 'text-winterblues-600' : ''}"
+			on:click={() => navigate(icon)}
+			use:tooltip
+			title={icons[icon]}
+		>
+			<Icon icon={icons[icon]} />
+		</div>
+	{/each}
 </div>
--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,6 +1,8 @@
 import { writable } from "svelte/store"
-import { supabase } from '$lib/supabaseClient.js'
+// import { supabase } from '$lib/supabaseClient.js'
 const settingsChange = writable({})
+
+export const currentView = writable('home')
 
 export const storedSettingsChange = {
     subscribe: settingsChange.subscribe,
@@ -11,28 +13,3 @@ export const storedSettingsChange = {
     }
 };
 
-export const loadQuotes = async () => {
-    const { data, error } = await supabase.from('quotes').select('*').run()
-    // console.log(`🚀 ~ file: stores.js ~ line 16 ~ quotes`, data)
-    if (error) {
-        return console.error(error)
-    }
-    return data
-}
-
-export const addQuote = async (quote) => {
-    const { data, error } = await supabase.from('quotes').insert(quote)
-    // quotes.set([...quotes.get(), quote])
-    quotes.update((cur) => [...cur, data[0]])
-}
- 
-export const deleteQuote = (id) => {
-    supabase.from('quotes').where('id', id).delete().run()
-    const {error} = supabase.from('quotes').delete().match({id})
-    quotes.update((cur) => {
-        const newQuotes = cur.filter(quote => quote.id !== id)
-        return newQuotes
-    })
-} 
-
-// toggleTodoCompleted @ 13:30 in video https://www.youtube.com/watch?v=YqIyET7XKIQ
