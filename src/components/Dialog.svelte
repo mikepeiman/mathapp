09ec1ec09379d61fa2f89b03a674ca1a0cb54234
Mike Peiman <mikepeiman@gmail.com>
2022-04-02 17:49:06 -0400

    Transition wrapper component not seeming to take effect; neither is Svelte transition:fade

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -4,7 +4,8 @@
 		DialogOverlay,
 		DialogTitle,
 		DialogDescription,
-        Transition
+        Transition,
+        TransitionChild
 	} from '@rgossiaux/svelte-headlessui';
     export let isOpen = true;
     $: console.log(`🚀 ~ file: Dialog.svelte ~ line 10 ~ isOpen`, isOpen)
@@ -13,18 +14,26 @@
 
 <Transition
 	show={isOpen}
-	enter="transition duration-100 ease-out"
+	enter="transition duration-500 ease-out"
 	enterFrom="transform scale-95 opacity-0"
 	enterTo="transform scale-100 opacity-100"
-	leave="transition duration-75 ease-out"
+	leave="transition duration-500 ease-out"
 	leaveFrom="transform scale-100 opacity-100"
 	leaveTo="transform scale-95 opacity-0"
 >
 	<Dialog on:close={() => (isOpen = false)}>
+        <TransitionChild
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
 		<DialogOverlay
 			class="p-3  w-full h-full fixed top-0 left-0 text-white"
 		/>
-
+        </TransitionChild>
 		<div class="flex flex-col w-full h-full bg-black bg-opacity-75 fixed items-center justify-center top-0 left-0 p-20 lg:p-[15%] 2xl:p-[25%]">
 			<DialogTitle>Deactivate account</DialogTitle>
 			<DialogDescription>This will permanently deactivate your account</DialogDescription>
--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -5,7 +5,16 @@
 	import { get } from 'svelte/store';
 	import { page } from '$app/stores';
 	import Auth from './Auth.svelte';
-	import Dialog from './Dialog.svelte';
+	import DialogComp from './Dialog.svelte';
+	import {
+		Dialog,
+		DialogOverlay,
+		DialogTitle,
+		DialogDescription,
+        Transition,
+        TransitionChild
+	} from '@rgossiaux/svelte-headlessui';
+	import { fade} from 'svelte/transition'
 	console.log(`🚀 ~ file: TopNav.svelte ~ line 7 ~ page`, $page.url);
 	$: current = get(currentView);
 	$: isOpen = true;
@@ -32,7 +41,8 @@
 
 <div id="header" class="grid w-full  items-center justify-center bg-winterblues-900">
 	<div class="logo">
-		<div class="flex">
+		{#if isOpen}
+		<div class="flex" transition:fade>
 			<h1
 				class="text-lg xl:text-xl text-center p-2 mx-2 rounded bg-fuchsia-700 hover:bg-fuchsia-500 cursor-pointer"
 				on:mousedown={() => handleDialog()}
@@ -40,8 +50,19 @@
 			>
 				Dialog
 			</h1>
-			<Dialog isOpen={isOpen} />
+			<Transition
+	show={isOpen}
+	enter="transition duration-500 ease-out"
+	enterFrom="transform scale-95 opacity-0"
+	enterTo="transform scale-100 opacity-100"
+	leave="transition duration-500 ease-out"
+	leaveFrom="transform scale-100 opacity-100"
+	leaveTo="transform scale-95 opacity-0"
+>
+			<DialogComp isOpen={isOpen} static />
+			</Transition>
 		</div>
+		{/if}
 	</div>
 	<div class="nav flex items-center justify-center">
 		{#each icons as icon}
