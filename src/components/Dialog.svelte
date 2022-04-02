2c26ed2db5aa6fa799d9b77e7921f29cf2baa19b
Mike Peiman <mikepeiman@gmail.com>
2022-04-02 18:24:46 -0400

    a bit of styling on modal

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -34,15 +34,17 @@
 			class="p-3  w-full h-full fixed top-0 left-0 text-white"
 		/>
         </TransitionChild>
-		<div class="flex flex-col w-full h-full bg-black bg-opacity-75 fixed items-center justify-center top-0 left-0 p-20 lg:p-[15%] 2xl:p-[25%]">
-			<DialogTitle>Deactivate account</DialogTitle>
-			<DialogDescription>This will permanently deactivate your account</DialogDescription>
+		<div class="flex flex-col w-full h-full bg-black bg-opacity-100 fixed items-center justify-center top-0 left-0 p-20 lg:p-[15%] 2xl:p-[25%]">
+			<!-- <DialogTitle>Sign up the Math App!</DialogTitle> -->
+			<div class="text-3xl font-bold text-sky-300 p-2">Sign up to the Math App!</div>
+			<DialogDescription>Easy, quick, and free (for now)</DialogDescription>
 			<p>
-				Are you sure you want to deactivate your account? All of your data will be permanently
-				removed. This action cannot be undone.
-			</p>
-			<button on:click={() => (isOpen = false)}>Deactivate</button>
-			<button on:click={() => (isOpen = false)}>Cancel</button>
+				The easiest way to generate math worksheets for your students and children.
+			</p><div class="flex">
+                
+                <button class="text-lg xl:text-xl text-center p-2 mx-2 rounded bg-sky-700 hover:bg-sky-500 cursor-pointer" on:click={() => (isOpen = false)}>Sign Up</button>
+                <button class="text-lg xl:text-xl text-center p-2 mx-2 rounded bg-amber-700 hover:bg-amber-500 cursor-pointer" on:click={() => (isOpen = false)}>Cancel</button>
+            </div>
 		</div>
 
 		<!-- ... -->
