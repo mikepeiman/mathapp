5f37dc25027b3c4921914e4f42118657afc1f67a
Mike Peiman <mikepeiman@gmail.com>
2022-04-28 12:53:33 -0400

    added basic privacy policy

---
+++ src/routes/privacy/index.svelte
@@ -0,0 +1,5 @@
+<h1>This is the privacy policy. It is a legal agreement between you and the website owner.</h1>
+<p>We do not use your social media profile or data for any purpose other than as a tool to validate and authorize your account (meaning, to log you in).</p>
+<p>We do not get access to any of your personal data. None. We use a simple API provided by the social media platforms.</p>
+<p>That's it. That's all.</p>
+
