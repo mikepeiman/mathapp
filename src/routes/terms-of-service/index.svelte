63bdff974c51598b9ef58253620aa8132781647f
Mike Peiman <mikepeiman@gmail.com>
2022-05-04 12:14:49 -0400

    added terms-of-service

---
+++ src/routes/terms-of-service/index.svelte
@@ -0,0 +1,5 @@
+<h1>These are our Terms Of Service: We (anyone involved with MathApp) are not responsible for any loss of data or other costs or losses associated with using our app.</h1>
+<p>Though, how on earth using a math app could cause you a loss is a bit of a strecth.</p>
+<p>Have a great day.</p>
+<p>That's it. That's all.</p>
+
